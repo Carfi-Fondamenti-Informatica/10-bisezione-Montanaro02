e5784f00f1037b2cb6f7e87715408b3a37f761f8
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-05-22 19:39:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,39 +2,39 @@
 #include <math.h>
 using namespace std;
 
+ 
+float f (float x){
+     float y;
+     y= (pow(x,2))*(cos(x))+1;
+     return y;
+ }
 
-float f (float n){
-    float y;
-    y= (pow(n,2))*(cos(n))+1;
-    return y;
-}
+ int main(){
+     float a,b,n;
 
-int main(){
-    float a,b,x;
+     do{
+         cout << "inserire estremi" << endl;
+         cin>>a;
+         cin>>b;
+     } while(f(a) * f(b)>=0 || a>b);
 
-    do{
-        cout << "inserire estremi" << endl;
-        cin>>a;
-        cin>>b;
-    } while(f(a) * f(b)>=0 || a>b);
+     float err;
 
-    float err;
+     do {
+         n =(a+b)/2;
+         if(f(n)==0.0){
+             break ;
+         }
+         else if(f(a) * f(n)>0){
+             a=n;
+         }else{
+             b=n;
+         }
+         err=abs((b-a)/2);
+     }while(err > 1e-6);
 
-    do {
-        x =(a+b)/2;
-        if(f(x)==0.0){
-            break ;
-        }
-        else if(f(a) * f(x)>0){
-            a=x;
-        }else{
-            b=x;
-        }
-        err =abs((b-a)/2);
-    }while(err > 1e-6);
+     cout << int(n*10000)/10000.0;
 
-    cout << int(x*10000)/10000.0<<"\n";
-    cout<<"fx"<< f(x*10000/10000.0);
-    return 0;
+     return 0;
 
-}
+ }
